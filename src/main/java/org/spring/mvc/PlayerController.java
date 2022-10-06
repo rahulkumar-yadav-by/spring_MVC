379cc9190a500d156ef78e2ffe3c5901a98cc6ac
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-10-06 16:20:42 +0530

    Post and Get Player

---
+++ src/main/java/org/spring/mvc/PlayerController.java
@@ -0,0 +1,26 @@
+package org.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PlayerController {
+
+    @RequestMapping(value="/searchPlayerForm")
+    public String searchPlayerForm(){
+
+        return "search-player-form";
+    }
+
+    @RequestMapping(value ="/showPlayerDetail" )
+    public String showPlayerDetail(HttpServletRequest request, Model model){
+        String pName=request.getParameter("playerName");
+        model.addAttribute("name",pName);
+        return "player-detail";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/player-detail.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+<head><title>Player Detail</title></head>
+<body>
+<h1>Player Detail</h1>
+    <p>Player Name is : <b> ${name} </b></p>
+</body>
+</html>
